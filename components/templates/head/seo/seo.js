fdbba820dcec48798b01d015aaf25203066e53ea
Richard Lovell <richjavalabs@gmail.com>
2021-12-14 22:41:02 +1300

    Updated layout settings

--- components/templates/head/seo/seo.js
+++ components/templates/head/seo/seo.js
@@ -1,32 +1,36 @@
 import Head from "next/head";
 
 export default function Seo({ content }) {
-  let { data } = content;
+  let { data } = { ...content };
   return (
-    <Head>
-      {data.title && (
-        <>
-          <title>{data.title}</title>
-          <meta property="og:title" content={data.title} />
-          <meta name="twitter:title" content={data.title} />
-        </>
+    <>
+      {data && (
+        <Head>
+          {data.title && (
+            <>
+              <title>{data.title}</title>
+              <meta property="og:title" content={data.title} />
+              <meta name="twitter:title" content={data.title} />
+            </>
+          )}
+          {data.description && (
+            <>
+              <meta name="description" content={data.description} />
+              <meta property="og:description" content={data.description} />
+              <meta name="twitter:description" content={data.description} />
+            </>
+          )}
+          {data.shareImage && (
+            <>
+              <meta property="og:image" content={data.shareImage} />
+              <meta name="twitter:image" content={data.shareImage} />
+              <meta name="image" content={data.shareImage} />
+            </>
+          )}
+          {data.article && <meta property="og:type" content="article" />}
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
       )}
-      {data.description && (
-        <>
-          <meta name="description" content={data.description} />
-          <meta property="og:description" content={data.description} />
-          <meta name="twitter:description" content={data.description} />
-        </>
-      )}
-      {data.shareImage && (
-        <>
-          <meta property="og:image" content={data.shareImage} />
-          <meta name="twitter:image" content={data.shareImage} />
-          <meta name="image" content={data.shareImage} />
-        </>
-      )}
-      {data.article && <meta property="og:type" content="article" />}
-      <meta name="twitter:card" content="summary_large_image" />
-    </Head>
+    </>
   );
 }
