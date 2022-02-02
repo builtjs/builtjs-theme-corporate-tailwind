df2a2e307a14acfd50a6a7fa9164ca2c5eee1fe6
Richard Lovell <richjavalabs@gmail.com>
2022-02-02 22:36:47 +1300

    Clean up

--- components/templates/headers/header-1/header-1.js
+++ components/templates/headers/header-1/header-1.js
@@ -7,16 +7,6 @@ import { ButtonLink } from "@/elements";
 import ModeToggleBtn from "../../../elements/mode-toggle-btn";
 
 export default function Header1({ content }) {
-  console.log('content--->', content);
-//   global:
-// logo:
-// ext: "svg"
-// height: 45
-// name: "builtjs"
-// path: "/images"
-// url: "/images/builtjs.svg"
-// width: 45
-// name: "Corporate Tailwind"
   let { attributes, collections, global } = { ...content };
   const { publicRuntimeConfig } = getConfig();
   const collectionNames = {
@@ -28,7 +18,6 @@ export default function Header1({ content }) {
   }
   const router = useRouter();
   const [navbarOpen, setNavbarOpen] = React.useState(false);
-console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attributes.url}`);
   return (
     <header id="header-1" className="template">
       <section className="p-4 border-b border-gray-200 dark:border-gray-700">
@@ -39,14 +28,18 @@ console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attrib
                 <span className="relative w-10">
                   <Image
                     className="text-gray-400 bg-white fill-current dark:bg-gray-800"
-                    src={`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attributes.url}`}
+                    src={`${publicRuntimeConfig.BACKEND_URL || ""}${
+                      global?.logo?.data.attributes.url
+                    }`}
                     width={global?.logo?.data.attributes.width}
                     height={global?.logo?.data.attributes.height}
                     layout="responsive"
                     alt=""
                   />
                 </span>
-                <span className="ml-3 text-xl font-bold text-black uppercase dark:text-white">{global.name}</span>
+                <span className="ml-3 text-xl font-bold text-black uppercase dark:text-white">
+                  {global.name}
+                </span>
               </a>
             </Link>
             <ul
@@ -58,7 +51,14 @@ console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attrib
               {menuItems &&
                 menuItems.map((menuItem) => {
                   return (
-                    <li key={menuItem.attributes.label} className={router.pathname == menuItem.attributes.url ? "active" : ""}>
+                    <li
+                      key={menuItem.attributes.label}
+                      className={
+                        router.pathname == menuItem.attributes.url
+                          ? "active"
+                          : ""
+                      }
+                    >
                       <Link href={menuItem.attributes.url}>
                         <a className="flex justify-center w-full p-3 text-gray-600 transition duration-200 ease-in-out rounded-md dark:text-gray-300 hover:text-gray-900 hover:bg-gray-100 dark:hover:text-gray-800 md:w-auto">
                           {menuItem.attributes.label}
@@ -71,10 +71,16 @@ console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attrib
 
             <ModeToggleBtn />
 
-            <button className="ml-4 button md:hidden hover:cursor-pointer" onClick={() => setNavbarOpen(!navbarOpen)}>
+            <button
+              className="ml-4 button md:hidden hover:cursor-pointer"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
               <div className="relative w-12 h-12 bg-black transition-colors duration-200 ease-in-out rounded-md hover:bg-gray-800">
                 <svg
-                  className={"p-3 w-12 absolute top-0 left-0 transition-opacity duration-200 ease-in-out" + (navbarOpen ? " opacity-0" : " opacity-100")}
+                  className={
+                    "p-3 w-12 absolute top-0 left-0 transition-opacity duration-200 ease-in-out" +
+                    (navbarOpen ? " opacity-0" : " opacity-100")
+                  }
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   height="100%"
@@ -89,7 +95,8 @@ console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attrib
                 </svg>
                 <svg
                   className={
-                    "py-3 pl-4 pr-2 w-12 absolute top-0 left-0 transition-opacity duration-200 ease-in-out" + (navbarOpen ? " opacity-100" : " opacity-0")
+                    "py-3 pl-4 pr-2 w-12 absolute top-0 left-0 transition-opacity duration-200 ease-in-out" +
+                    (navbarOpen ? " opacity-100" : " opacity-0")
                   }
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -97,7 +104,13 @@ console.log(`${publicRuntimeConfig.BACKEND_URL || ""}${global?.logo?.data.attrib
                   width="100%"
                   viewBox="0 0 24 16"
                 >
-                  <path d="M14.34 1L1 14.34m13.34 0L1 1l13.34 13.34z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path
+                    d="M14.34 1L1 14.34m13.34 0L1 1l13.34 13.34z"
+                    stroke="#fff"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
                 </svg>
               </div>
             </button>
