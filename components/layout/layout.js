505a5b255b35a1878d5243390e215f115d4df2e3
Richard Lovell <richjavalabs@gmail.com>
2022-06-24 16:51:25 +1200

    Chore: Code cleanup

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -19,26 +19,3 @@ const Layout = (props) => {
 };
 
 export default Layout;
-
-// const Layout = (props) => {
-//   const { children, page } = props;
-//   return (
-//     <>
-//       {page.layout && page.layout._0 && (
-//         <page.layout._0.component
-//           content={page.layout._0.content}
-//           app={page.app}
-//         />
-//       )}
-//       <main>{children}</main>
-//       {page.layout && page.layout._1 && (
-//         <page.layout._1.component
-//           content={page.layout._1.content}
-//           app={page.app}
-//         />
-//       )}
-//     </>
-//   );
-// };
-
-// export default Layout;
